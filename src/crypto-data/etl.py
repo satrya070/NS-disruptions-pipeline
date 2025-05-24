3b621a292d99048d02987cca7b88e5e0db957e4a
Satrya <s.sabeni17@gmail.com>
2025-05-24 11:18:13 +0200

    add config

--- src/crypto-data/etl.py
+++ src/crypto-data/etl.py
@@ -1,8 +1,14 @@
 import boto3
+import json
 
+from config import TRACKING_COINS
+from etl_util import process_spaces
 
 def extract():
-    print("data from api")
+    coins_list_param = [process_spaces(coin) for coin in TRACKING_COINS]  # process coinnames with spaces
+    coins_list_param = "%2C".join(coins_list_param)  # build string param with commas
+
+    coins_data = fetch_data(coin_list_params)
 
 def load():
     print("load data into sqlite")

7162ef49e3b8060e9d6f9a0dbbcb6cd6f6a9fd56
Satrya <s.sabeni17@gmail.com>
2025-05-24 20:25:47 +0200

    inserting into sqlite3 test

--- src/crypto_data/etl.py
+++ src/crypto_data/etl.py
@@ -1,4 +1,5 @@
 import boto3
+import sqlite3
 
 from crypto_data.config import TRACKING_COINS
 from crypto_data.apis.coingecko_api import CoinGeckoAPI
@@ -12,13 +13,22 @@ def extract_data() -> list[tuple]:
     
     return coin_api_data
 
-def load_data(coin_api_data: dict[str, dict]):
-    
-
+def load_data(coin_insert_data: dict[str, dict]):
+    """
+    insert data in sqlite db
+    """
     insert_coin_data_query = "INSERT INTO coins_data (name, price, change_24h, volume_24h, market_cap)" \
     "VALUES (?, ?, ?, ?, ?)"
 
+    try:
+        with sqlite3.connect("/home/satrya070/Documents/github/crypto-data/data/crypto-db.db") as conn:
+            cursor = conn.cursor()
+            cursor.executemany(insert_coin_data_query, coin_insert_data)
+            conn.commit()
+            cursor.close()
 
+    except sqlite3.Error as e:
+        raise e
 
 def transform():
     print("perform enhancement aggregations")

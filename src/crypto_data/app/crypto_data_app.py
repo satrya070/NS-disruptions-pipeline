35c4c6d4d16c822a851e63c4c92d244069e7dcd2
Satrya <s.sabeni17@gmail.com>
2025-05-26 20:37:53 +0200

    add alchemy, add working postgrs to streamlit

--- src/crypto_data/app/crypto_data_app.py
+++ src/crypto_data/app/crypto_data_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-
 st.title("Crypto Data")
 
+# init conn the streamlit way
+conn = st.connection("postgresql", type="sql")
+
+df = conn.query("SELECT * FROM public.coins_data")
+
 st.text("showing data..")
+
+for row in df.itertuples():
+    st.write(f"{row.name} is at rate: {row.price}")
